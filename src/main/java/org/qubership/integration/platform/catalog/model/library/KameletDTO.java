a775c4128e84e4bc33dbef479616b44ba7321081
anton-pecherskikh <anton.pecherskikh.qubership@gmail.com>
2025-03-21 09:24:21 +0300

    Added kamelet catalog rest endpoints

---
+++ src/main/java/org/qubership/integration/platform/catalog/model/library/KameletDTO.java
@@ -0,0 +1,22 @@
+package org.qubership.integration.platform.catalog.model.library;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+
+@Data
+@Schema(description = "Kamelet definition representing an integration template in Apache Camel.")
+public class KameletDTO {
+    @Schema(description = "API version of the Kamelet", example = "camel.apache.org/v1alpha1")
+    private String apiVersion;
+
+    @Schema(description = "Kind of the resource", example = "Kamelet")
+    private String kind;
+
+    @Schema(description = "Metadata containing the name and labels of the Kamelet")
+    private JsonNode metadata;
+
+    @Schema(description = "Specification of the Kamelet")
+    private JsonNode spec;
+}
---
+++ src/main/java/org/qubership/integration/platform/catalog/service/KameletStorageService.java
@@ -0,0 +1,72 @@
+package org.qubership.integration.platform.catalog.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.qubership.integration.platform.catalog.model.library.KameletDTO;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@Slf4j
+public class KameletStorageService {
+    private static final String KAMELETS_PATH = "classpath*:kamelets/*.kamelet.yaml";
+    private final ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+    private final ObjectMapper jsonMapper = new ObjectMapper();
+
+    @Getter
+    private final Map<String, KameletDTO> kamelets = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void loadKamelets() {
+        try {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource[] resources = resolver.getResources(KAMELETS_PATH);
+            for (Resource resource : resources) {
+                KameletDTO kameletDTO = yamlMapper.readValue(resource.getInputStream(), KameletDTO.class);
+                kamelets.put(String.valueOf(kameletDTO.getMetadata().get("name")).replaceAll("^\"|\"$", ""), kameletDTO);
+                log.info("Loaded kamelet: " + String.valueOf(kameletDTO.getMetadata().get("name")));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while Kamelets load", e);
+        }
+    }
+
+    public String getKameletByName(String name) {
+        KameletDTO kamelet = kamelets.get(name);
+        if (kamelet == null) {
+            log.warn("Kamelet '{}' hasn't found", name);
+        }
+        try {
+            return (kamelet != null) ? jsonMapper.writeValueAsString(kamelet) : null;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getKameletSpec(String name) {
+        KameletDTO kamelet = kamelets.get(name);
+        try {
+            return (kamelet != null) ? jsonMapper.writeValueAsString(kamelet.getSpec()) : null;
+        } catch (IOException e) {
+            throw new RuntimeException("Error serializing Kamelet Spec to JSON", e);
+        }
+    }
+
+    public String getKameletTemplate(String name) {
+        KameletDTO kamelet = kamelets.get(name);
+        try {
+            return (kamelet != null) ? jsonMapper.writeValueAsString(kamelet.getSpec().get("template")) : null;
+        } catch (IOException e) {
+            throw new RuntimeException("Error serializing Kamelet Template to JSON", e);
+        }
+    }
+}
